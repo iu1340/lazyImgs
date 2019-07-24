eb13fcdfca11da33067b5b8e6550eec115d6a51f
houtao02 <houtao02@appledeMacBook-Pro.local>
2019-07-24 14:10:40 +0800

    demo branch

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="hello">
+    <img v-for="(item,i) of images" :key="i" v-lazy="item.image" parent=".hello" width="100" height="100" />
+  </div>
+</template>
+
+<script>
+import images from '../assets/imagesurl.json'
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      images: images
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.hello{
+  width: 600px;
+  height: 500px;
+  overflow: auto
+}
+</style>
